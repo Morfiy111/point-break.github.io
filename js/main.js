99044465b6fd75d67522e71b6d80cc17b0a553ec
morfiy <keks46@yandex.ru>
2023-09-04 11:20:07 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -14,3 +14,28 @@ $(document).ready(function () {
     });
 });
 
+jQuery('.plit_imgs ul.plit_img_small li:first-child').addClass('small_act');
+jQuery('.plit_imgs ul.plit_img_small li a').on('click', function () {
+    jQuery('.plit_imgs ul.plit_img_small li').removeClass('small_act');
+    jQuery(this).parent('li').addClass('small_act');
+    jQuery('.plit_imgs ul.plit_img_big li').hide();
+    jQuery('.plit_imgs ul.plit_img_big li.' + jQuery(this).attr('id')).show();
+    jQuery('.plit_img_big li a img').each(function () {
+        /*var topact=(567*jQuery(this).height()/jQuery(this).width()-398);
+        if(topact>0)
+            jQuery(this).css({
+                'marginTop': '-'+(topact/2)+'px'
+            });*/
+    });
+
+    return false;
+});
+
+// scroll to child
+
+jQuery('.plit_content_list ul li a').on('click', function () {
+    jQuery('html, body').animate({
+        scrollTop: (jQuery('#sub_' + jQuery(this).attr('id')).offset().top) - 165
+    }, 1000);
+    return false;
+})
